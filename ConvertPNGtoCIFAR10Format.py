81f31c3a44ec7c539bf318acb156ab03a41172a0
Nitya-Matcha <159713644+Nitya-Matcha@users.noreply.github.com>
2025-01-12 15:57:23 -0500

    Update ConvertPNGtoCIFAR10Format.py

--- ConvertPNGtoCIFAR10Format.py
+++ ConvertPNGtoCIFAR10Format.py
@@ -1,3 +1,8 @@
 import os
 import numpy as np
+from PIL import Image
+import pickle
 
+def resizing(image_path, size = (32,32)):
+  image = Image.open(image_path).convert("RGB").resize(size)
+  return np.array(image)

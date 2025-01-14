daeced4d9fc70ec4f6b9c6bc3cfe2df061b2cad8
Nitya-Matcha <159713644+Nitya-Matcha@users.noreply.github.com>
2025-01-13 21:08:18 -0500

    Update ConvertPNGtoCIFAR10Format.py

--- ConvertPNGtoCIFAR10Format.py
+++ ConvertPNGtoCIFAR10Format.py
@@ -1,8 +1,21 @@
 import os
-import numpy as np
 from PIL import Image
 import pickle
+import numpy as np
 
-def resizing(image_path, size = (32,32)):
-  image = Image.open(image_path).convert("RGB").resize(size)
+def resizing(imagepath, size = (32,32)):
+  image = Image.open(imagepath).convert("RGB").resize(size)
   return np.array(image)
+
+def convertimagetocifar(imagedirectory, savefile, namestonumbers):
+  data = []
+  labels = []
+
+for labelnames, labelindex in label.items():
+  classdirectory = os.path.join(imagedirectory, labelnames)
+  if not os.path.isdir(classdirectory):
+    print("Not in this folder of images")
+  else:
+    continue
+
+

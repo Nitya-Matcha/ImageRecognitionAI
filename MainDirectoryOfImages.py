2692456ccae53a1eb4a5251ddce9e59b2b20229a
Nitya-Matcha <159713644+Nitya-Matcha@users.noreply.github.com>
2025-01-24 22:28:32 -0500

    Update MainDirectoryOfImages.py

--- MainDirectoryOfImages.py
+++ MainDirectoryOfImages.py
@@ -1,12 +1,15 @@
 import os
 import shutil
 
-os.mkdir(images, exist = True) 
+os.makedirs(images, exist = True) 
 os.cddir(images) 
 '''
 I'm pretty sure the above code that says os.cddir images is highkey wrong, 
 because it's supposed to contain brackets but I'm unsure whether I'm supposed to be calling the main directory or the subdirectories
 '''
-os.makedirs(Myocardial, Abnormal, History, Normal)
+# I'm making this a comment, I dont think this is how you make subdirectories os.makedirs(Myocardial, Abnormal, History, Normal)
 
 # Below I will be organizing the images into their respective folders, The first one will be Myocardial
+
+for name in names:
+  images = 

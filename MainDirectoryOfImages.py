3c92ab6bf5bffedd2d3763f3465365f2cfca817e
Nitya-Matcha <159713644+Nitya-Matcha@users.noreply.github.com>
2025-01-24 22:54:09 -0500

    Update MainDirectoryOfImages.py

--- MainDirectoryOfImages.py
+++ MainDirectoryOfImages.py
@@ -7,9 +7,15 @@ os.cddir(images)
 I'm pretty sure the above code that says os.cddir images is highkey wrong, 
 because it's supposed to contain brackets but I'm unsure whether I'm supposed to be calling the main directory or the subdirectories
 '''
-# I'm making this a comment, I dont think this is how you make subdirectories os.makedirs(Myocardial, Abnormal, History, Normal)
+#I'm making this a comment, I dont think this is how you make subdirectories os.makedirs(Myocardial, Abnormal, History, Normal)
 
-# Below I will be organizing the images into their respective folders, The first one will be Myocardial
+#Tbh the code below I got from an online source because like creating subdirectories for everything and their mom was kind of a pain
 
 for name in names:
-  images = 
+  classdirectory = os.path.join(images, name)
+  os.makedirs(classdirectory, exist = True)
+  for imagepaths in imagepathsforallclasses(name, []):
+    shutil.copy(imagepaths, classdirectory)
+else:
+  print("Sorry, couldn't copy image path :(")
+  
